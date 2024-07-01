5b0e34f10521aa9f14a555a9a3be5a1d8f689aa7
Daniel Ivtimov <danieliftimov3@gmail.com>
2024-07-01 23:53:20 +0200

    project

--- scripts/index.js
+++ scripts/index.js
@@ -1,232 +1,236 @@
 
-let currentTaskElapsedTime = 0
-let activeTaskIdPassed
-let topDivActiveTask = document.getElementById('topDivActiveTask')
-let timer = document.getElementById('timer')
-let addTaskButton = document.getElementById('addTaskButton')
-let startStopButton = document.getElementById('startStopButton')
-let taskNameBox = document.getElementById('taskName')
-let taskTimeBox = document.getElementById('taskTime')
-let taskBox = document.getElementById('taskBox')
-let currentActiveTaskId = -1
+let currentTaskElapsedTime = 0;
+let topDivActiveTask = document.getElementById('topDivActiveTask');
+let timer = document.getElementById('timer');
+let addTaskButton = document.getElementById('addTaskButton');
+let startStopButton = document.getElementById('startStopButton');
+let taskNameBox = document.getElementById('taskName');
+let taskTimeBox = document.getElementById('taskTime');
+let taskBox = document.getElementById('taskBox');
+let currentActiveTaskId = -1;
 
 let beforeRefreshingPage = (e) => {
     e.preventDefault();
 };
 
-window.addEventListener("beforeunload", beforeRefreshingPage)
+window.addEventListener("beforeunload", beforeRefreshingPage);
 
-showTasks()
-//Create task
+showTasks();
+
+// Create task
 addTaskButton.addEventListener('click', function () {
-    let itemsFromStirage = localStorage.getItem('tasks') //string
-    let tasks = JSON.parse(itemsFromStirage)
-    ids = []
-    let assignedId
-    if (taskNameBox.value !== '') {
-        if (taskTimeBox !== '') {
-            let pomodoros = parseFloat(taskTimeBox.value)
-            if (parseFloat(pomodoros) === false) {
-                alert('Please enter Pomodoros')
-            } else {
-                if (tasks === null || tasks.length === 0) {
-                    tasks = []
-                    assignedId = 1
-                    ids.push(assignedId)
-                } else {
-                    tasks.forEach(element => {
-                        ids.push(element.id)
-                    });
-                    let maxValue = Math.max(...ids);
-                    assignedId = maxValue + 1
-                    ids.push(assignedId)
-                }
-                taskTimeInMinutes = (pomodoros * 25).toFixed(2) //convert to minutes
-                let taskObject = { 'id': assignedId, 'nameOfTask': taskNameBox.value, 'time': taskTimeInMinutes, 'remainingTime': taskTimeInMinutes * 60, 'elapsedTime': 0, 'totalPomodoros': pomodoros, 'remainingPomodoros': 0 }
+    let itemsFromStorage = localStorage.getItem('tasks');
+    let tasks = JSON.parse(itemsFromStorage) || [];
+    let ids = [];
 
-                tasks.push(taskObject)
-                taskTimeBox.value = ''
-                taskNameBox.value = ''
-                localStorage.setItem("tasks", JSON.stringify(tasks))
-                showTasks()
+    // Task Limit (1-5)
+    if (tasks.length >= 5) {
+        alert('You can only add up to 5 tasks.');
+        return;
+    }
+
+    if (taskNameBox.value !== '') {
+        if (!isNaN(taskTimeBox.value) && taskTimeBox.value !== '') {
+            let pomodoros = parseFloat(taskTimeBox.value);
+            
+            if (tasks.length === 0) {
+                assignedId = 1;
+                ids.push(assignedId);
+            } else {
+                tasks.forEach(element => {
+                    ids.push(element.id);
+                });
+                let maxValue = Math.max(...ids);
+                assignedId = maxValue + 1;
+                ids.push(assignedId);
+            }
+    
+            taskTimeInMinutes = (pomodoros * 25).toFixed(2); // convert to minutes
+            let taskObject = {
+                'id': assignedId,
+                'nameOfTask': taskNameBox.value,
+                'time': taskTimeInMinutes,
+                'remainingTime': taskTimeInMinutes * 60,
+                'elapsedTime': 0,
+                'totalPomodoros': pomodoros,
+                'remainingPomodoros': 0
+            };
+            tasks.push(taskObject);
+            taskTimeBox.value = '';
+            taskNameBox.value = '';
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            showTasks();
+            if (tasks.length >= 5) {
+                addTaskButton.disabled = true;
             }
         } else {
-            alert('Please enter Pomodoros')
+            alert('Please enter a valid number for Pomodoros.');
         }
     } else {
-        alert('Please enter title')
+        alert('Please enter a title.');
     }
-})
+});
 
-
-//Remove task
+// Remove task
 document.addEventListener('click', function (e) {
-    let itemsFromStirage = localStorage.getItem('tasks') //string
-    let tasks = JSON.parse(itemsFromStirage)
+    let itemsFromStorage = localStorage.getItem('tasks');
+    let tasks = JSON.parse(itemsFromStorage) || [];
     if (e.target.classList.contains('removeButton')) {
-        let removeId = tasks.findIndex(object => { return object.id == e.target.parentNode.id })
+        let removeId = tasks.findIndex(object => { return object.id == e.target.parentNode.id });
 
         if (confirm('Are you sure that you want to remove the task?')) {
-            tasks.splice(removeId, 1)
-            e.target.remove()
+            tasks.splice(removeId, 1);
             localStorage.setItem("tasks", JSON.stringify(tasks));
-            for (i = 0; i < tasks.length; i++) {
-                console.log(`Remainng task is: ${tasks[i].nameOfTask} with time ${tasks[i].time} with id: ${tasks[i].id}`)
+            showTasks();
+
+            if (tasks.length < 5) {
+                addTaskButton.disabled = false;
             }
-            showTasks()
         }
     }
 }, false);
 
-//Set timer
+// Set timer
 document.addEventListener('click', function (e) {
     if (e.target.classList.contains('set_timer')) {
-        itemsFromStorage = localStorage.getItem('tasks')
-        tasks = JSON.parse(itemsFromStorage)
-        activeTaskId = e.target.parentNode.id
-        let getActiveTaskIndex = tasks.findIndex(object => { return object.id == activeTaskId })
-        let activeTask = tasks[getActiveTaskIndex] //Get specific task object
+        let itemsFromStorage = localStorage.getItem('tasks');
+        let tasks = JSON.parse(itemsFromStorage) || [];
 
-        topDivActiveTask.innerHTML = activeTask.nameOfTask
-        currentActiveTaskId = activeTask.id
-        let screenMinutes = '00'
-        let screenSeconds = '00'
+        let activeTaskId = parseInt(e.target.parentNode.id);
+        if (!isNaN(activeTaskId)) {
+            let activeTask = tasks.find(task => task.id === activeTaskId);
 
-        let activeTaskRemainingTime = activeTask.remainingTime //taken task time in seconds
-        if (activeTaskRemainingTime != 0) {
-            if (activeTaskRemainingTime >= 1500) {
-                screenMinutes = '25'
-                screenSeconds = '00'
-            } else if (600 <= activeTaskRemainingTime < 1500) {
-                screenMinutes = `${Math.floor(activeTaskRemainingTime / 60)}`
-                if (activeTaskRemainingTime % 60 < 10) {
-                    screenSeconds = `0${activeTaskRemainingTime % 60}`
-                } else {
-                    screenSeconds = `${activeTaskRemainingTime % 60}`
-                }
-            } else {
-                screenMinutes = `${Math.Floor(activeTaskRemainingTime / 60)}`
-                if (activeTaskRemainingTime % 60 < 10) {
-                    screenSeconds = `0${activeTaskRemainingTime % 60}`
-                } else {
-                    screenSeconds = `${activeTaskRemainingTime % 60}`
-                }
+            if (activeTask) {
+                topDivActiveTask.innerHTML = activeTask.nameOfTask;
+                currentActiveTaskId = activeTask.id;
+                timer.innerHTML = '25:00'; 
+
+                showTasks();
             }
-
-            timer.innerHTML = `${screenMinutes}:${screenSeconds}`
         }
     }
-    showTasks()
 }, false);
 
-//Start/Pause timer
-document.addEventListener('click', function (e) {
-    // timeTaskIsActive = 0
-    if (topDivActiveTask.innerText !== 'No Active Task') {
-        if (e.target.id === 'startStopButton') {
-            if (e.target.innerText === 'Start') {
-                addTaskButton.hidden = true
-                myInterval = setInterval(trackAndUpdateTimer, 1000)
-                e.target.innerText = 'Pause'
-            }
-            else if (e.target.innerText === 'Pause') {
-                addTaskButton.hidden = false
-                clearInterval(myInterval)
+// Start/Pause timer
+startStopButton.addEventListener('click', function (e) {
+    if (e.target.innerText === 'Start') {
+        let currentTimeOnTimer = timer.innerHTML;
+        let [minutes, seconds] = currentTimeOnTimer.split(':').map(part => parseInt(part, 10));
 
-                itemsFromStorage = localStorage.getItem('tasks')
-                tasks = JSON.parse(itemsFromStorage)
-                activeTaskId = e.target.parentNode.id
-                let getActiveTaskIndex = tasks.findIndex(object => { return object.id == currentActiveTaskId })
-                let activeTask = tasks[getActiveTaskIndex] //Get specific task object
-                activeTaskIdPassed = activeTask
-                activeTask.remainingTime = activeTask.remainingTime - currentTaskElapsedTime
-                localStorage.setItem("tasks", JSON.stringify(tasks))
-                currentTaskElapsedTime = 0
-                e.target.innerText = 'Start'
-            }
+        if (minutes === 25 && seconds === 0) {
+            e.target.innerText = 'Pause';
+            addTaskButton.hidden = true;
+            myInterval = setInterval(trackAndUpdateTimer, 1000);
+        } else {
+            alert('Please set the timer before starting.');
         }
+    } else if (e.target.innerText === 'Pause') {
+        e.target.innerText = 'Start';
+        addTaskButton.hidden = false;
+        clearInterval(myInterval);
     }
-})
+});
 
 function trackAndUpdateTimer() {
-    currentTaskElapsedTime++
-    currentTime = timer.innerHTML
-    prints = currentTime.split(':')
-    minutes = prints[0]
-    seconds = prints[1]
+    currentTaskElapsedTime++;
+    currentTime = timer.innerHTML;
+    prints = currentTime.split(':');
+    minutes = prints[0];
+    seconds = prints[1];
     if (minutes == 0 && seconds == 0) {
-        clearInterval(myInterval)
+        clearInterval(myInterval);
 
-        itemsFromStorage = localStorage.getItem('tasks')
-        tasks = JSON.parse(itemsFromStorage)
-        let getActiveTaskIndex = tasks.findIndex(object => { return object.id == currentActiveTaskId })
-        tasks[getActiveTaskIndex].remainingTime = tasks[getActiveTaskIndex].remainingTime - currentTaskElapsedTime
+        let itemsFromStorage = localStorage.getItem('tasks');
+        let tasks = JSON.parse(itemsFromStorage) || [];
+        let getActiveTaskIndex = tasks.findIndex(object => { return object.id == currentActiveTaskId });
 
-        currentTaskElapsedTime = 0
-        //
-        if (tasks[getActiveTaskIndex].remainingTime > 0) {
-            alert('Pomodoro finished. Take a break :-)')
-            startStopButton.innerText = 'Start'
-            timer.innerHTML = '25:00'
-            topDivActiveTask.innerText = 'No Active Task'
-            addTaskButton.hidden = false
+        if (getActiveTaskIndex !== -1) {
+            tasks[getActiveTaskIndex].remainingTime -= currentTaskElapsedTime;
             localStorage.setItem("tasks", JSON.stringify(tasks));
-            showTasks()
 
-        } else if(tasks[getActiveTaskIndex].remainingTime <= 0) {
-            tasks.splice(getActiveTaskIndex, 1)
-
-            alert('Task focus is finished. Take a break :-)')
-            startStopButton.innerText = 'Start'
-            timer.innerHTML = '25:00'
-            topDivActiveTask.innerText = 'No Active Task'
-            addTaskButton.hidden = false
-            localStorage.setItem("tasks", JSON.stringify(tasks));
-            showTasks()
+            if (tasks[getActiveTaskIndex].remainingTime > 0) {
+                alert('Pomodoro finished. Take a break :-)');
+                startStopButton.innerText = 'Start';
+                timer.innerHTML = '25:00';
+                topDivActiveTask.innerText = 'No Active Task';
+                addTaskButton.hidden = false;
+            } else {
+                tasks.splice(getActiveTaskIndex, 1);
+                alert('Task focus is finished. Take a break :-)');
+                startStopButton.innerText = 'Start';
+                timer.innerHTML = '25:00';
+                topDivActiveTask.innerText = 'No Active Task';
+                addTaskButton.hidden = false;
+                localStorage.setItem("tasks", JSON.stringify(tasks));
+            }
         }
-        //
+
+        currentTaskElapsedTime = 0;
+        showTasks();
     }
-    seconds--
+
+    seconds--;
     if (seconds >= 0) {
-        if (seconds < 10) {
-            timer.innerHTML = `${minutes}:0${seconds}`
-        } else {
-            timer.innerHTML = `${minutes}:${seconds}`
-        }
+        timer.innerHTML = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
     } else {
         if (minutes > 0) {
-            minutes--
-            seconds = 59
-            if (seconds < 10) {
-                timer.innerHTML = `${minutes}:0${seconds}`
-            } else {
-                timer.innerHTML = `${minutes}:${seconds}`
-            }
+            minutes--;
+            seconds = 59;
+            timer.innerHTML = `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
         }
     }
 }
-
 
 
 function showTasks() {
-    let itemsFromStirage = localStorage.getItem('tasks') //string
-    let tasks = JSON.parse(itemsFromStirage) //object
-    taskBox.innerHTML = ''
-    if (tasks) {
-        for (i = 0; i < tasks.length; i++) {
+    let itemsFromStorage = localStorage.getItem('tasks');
+    let tasks = JSON.parse(itemsFromStorage) || [];
+    taskBox.innerHTML = '';
+    
+    if (tasks.length > 0) {
+        tasks.forEach(task => {
+            let importantCheckbox = task.important ? 'checked' : ''; 
             taskBox.innerHTML += `
-                <div id="${tasks[i].id}" class="card ${tasks[i].id}" style="width: 18rem;">
-                    <div class="card-body ${tasks[i].id}">
-                        <h5 class="card-title ${tasks[i].id}">${tasks[i].nameOfTask}</h5>
+                <div id="${task.id}" class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${task.nameOfTask}</h5>
                     </div>
-                    <ul class="list-group list-group-flush ${tasks[i].id}">
-                        <li class="list-group-item ${tasks[i].id}">${tasks[i].time} min</li>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">${task.time} min</li>
                     </ul>
-                    <div id="${tasks[i].id}" class="center">
+                    <div class="center">
                         <button type="button" class="btn btn-secondary btn-sm set_timer">Set Timer</button>
                         <button type="button" class="removeButton btn btn-secondary btn-sm">Remove</button>
+                        <input type="checkbox" class="importantCheckbox" data-taskid="${task.id}" ${importantCheckbox}>
+                        <label for="importantCheckbox">Mark as Important</label>
                     </div>
-                </div><br/>`
-        }
+                </div><br/>
+            `;
+        });
     }
+
+    if (tasks.length >= 5) {
+        addTaskButton.disabled = true;
+    } else {
+        addTaskButton.disabled = false;
+    }
+    
+    let importantCheckboxes = document.querySelectorAll('.importantCheckbox');
+    importantCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('click', function() {
+            let taskId = parseInt(checkbox.getAttribute('data-taskid'));
+            let taskToUpdate = tasks.find(task => task.id === taskId);
+            if (taskToUpdate) {
+                taskToUpdate.important = checkbox.checked;
+                localStorage.setItem('tasks', JSON.stringify(tasks));
+                showTasks(); 
+            }
+        });
+    });
 }
+
+
+
+
+
+
